b5d57d470ff41ed95d989167f30dd73a701678b7
Pelzmagic <83060434+pelzmagic@users.noreply.github.com>
2022-04-21 21:06:09 -0700

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
- * add_node - adds a node to a linked list head
+ * *add_node - adds a node to a linked list head
  * @head: linked list head
  * @str: string element of the linked list
  * Return: number of elements
@@ -27,8 +27,8 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-	for (len_str = 0; str[len_str] != '\0'; len_str++)
-		;
+	for (len_str = 0; str[len_str] != '\0';  len_str++)
+	;
 
 	new_node->len = len_str;
 	new_node->next = *head;
